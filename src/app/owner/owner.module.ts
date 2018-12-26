00ff9d5f4a3e40dd26611bae1a048f449bc7ed7c
Augustine Chow <aug_chow00@hotmail.com>
2018-12-26 12:08:31 +0800

    Complete tutorial pt12

--- src/app/owner/owner.module.ts
+++ src/app/owner/owner.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { OwnerListComponent } from './owner-list/owner-list.component';
 import { RouterModule } from '@angular/router';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { OwnerDetailsComponent } from './owner-details/owner-details.component';
     RouterModule.forChild([
       { path: 'list', component: OwnerListComponent },
       { path: 'details/:id', component: OwnerDetailsComponent }
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class OwnerModule { }
